b6b360cbdc9e57b10e141ba4573460387e3ed8ba
regis667 <regis667@gmail.com>
2021-11-21 16:39:30 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ sha256sum bitcoin-0.21.0-x86_64-linux-gnu.tar.gz >> sum
 echo "downloaded sha256sum: " && cat sum
 echo "correct sha256sum: " && grep bitcoin-0.21.0-x86_64-linux-gnu.tar.gz SHA256SUMS.asc
 tar -xf bitcoin-0.21.0-x86_64-linux-gnu.tar.gz
+install -m 0755 -o root -g root -t /usr/local/bin bitcoin-22.0/bin/*
+bitcoind -daemon
