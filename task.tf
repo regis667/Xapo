16863bc85d47f72b1fa458904bc82d5aa2da5040
regis667 <regis667@gmail.com>
2021-11-21 18:00:54 +0100

    Create task.tf

---
+++ task.tf
@@ -0,0 +1,25 @@
+resource "aws_ecs_task_definition" "xapo_task" {
+  family                   = "xapo-task"
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "xapo_task",
+      "image": "xapo_docker_image",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 8333,
+          "hostPort": 8333
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"   
+  memory                   = 512         
+  cpu                      = 256         
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
