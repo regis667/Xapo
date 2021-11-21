d278482c4e2ca865b0cf8be2175c32b03b6d527f
regis667 <regis667@gmail.com>
2021-11-21 16:18:00 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@
 sha256sum bitcoin-0.21.0-aarch64-linux-gnu.tar.gz >> sum
 echo "downloaded sha256sum: " && cat sum
 echo "correct sha256sum: " && grep bitcoin-0.21.0-aarch64-linux-gnu.tar.gz.1 SHA256SUMS.asc
+tar -xf bitcoin-0.21.0-aarch64-linux-gnu.tar.gz
