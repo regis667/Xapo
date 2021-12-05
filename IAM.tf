f82d7135ef8156a6ab692154aaf0ea029cf59d09
regis667 <regis667@gmail.com>
2021-12-05 12:50:50 +0100

    Update IAM.tf

--- IAM.tf
+++ IAM.tf
@@ -1,24 +1,55 @@
-resource "aws_iam_role" "Xapo_role" {
-  name = "Xapo_role"
+resource "aws_iam_user" "user" {
+  name = "test-user"
+}
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
+resource "aws_iam_role" "role" {
+  name = "test-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
        "Principal": {
          "AWS": [
-           "arn:aws:iam::AWS-account-ID:user/user-name-1", 
-           "arn:aws:iam::AWS-account-ID:user/user-name-2"
-				]
-					}
-      },
-				]
-								})
+           "arn:aws:iam::AWS-account-ID:user/*},
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
 
-  tags = {
-    tag-key = "tag-value"
-  }
+resource "aws_iam_group" "group" {
+  name = "test-group"
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A xapo_task policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+       "sts:AssumeRole"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  users      = [aws_iam_user.user.name]
+  roles      = [aws_iam_role.role.name]
+  groups     = [aws_iam_group.group.name]
+  policy_arn = aws_iam_policy.policy.arn
 }
